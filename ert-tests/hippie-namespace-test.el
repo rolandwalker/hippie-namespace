de17a59fda6111b174323934610056e828fe66f6
Roland Walker <walker@pobox.com>
2012-10-13 22:30:27 -0400

    comment

--- ert-tests/hippie-namespace-test.el
+++ ert-tests/hippie-namespace-test.el
@@ -1,9 +1,57 @@
 
-;; requires and setup
+;;; requires and setup
 
 (require 'hippie-namespace)
 
 ;;; this is a stub - no tests defined
+
+;;; he-namespace-beg ()
+
+;;; try-expand-namespace (old)
+
+;;; hippie-namespace-buffer-included-p (buf)
+
+;;; hippie-namespace-list-flatten (list)
+
+;;; hippie-namespace-prefix-popular-p (prefix collection cutoff)
+
+;;; hippie-namespace-longest-prefix (collection cutoff)
+
+;;; hippie-namespace-strip-prefix-matches (collection)
+
+;;; hippie-namespace-all-symbols ()
+
+;;; hippie-namespace-all-imenu-definitions ()
+
+;;; hippie-namespace-generic-plugin (&optional method)
+
+;;; hippie-namespace-plugin-lisp-mode ()
+
+;;; hippie-namespace-plugin-perl-mode ()
+
+;;; hippie-namespace-plugin-python-mode ()
+
+;;; hippie-namespace-plugin-ruby-mode ()
+
+;;; hippie-namespace-plugin-c++-mode ()
+
+;;; hippie-namespace-plugin-php-mode ()
+
+;;; hippie-namespace-plugin-java-mode ()
+
+;;; hippie-namespace-plugin-go-mode ()
+
+;;; hippie-namespace-populate-list (&optional force)
+
+;;; hippie-namespace-mark-symbol-portion ()
+
+;;; hippie-namespace-maybe-turn-on (&optional arg)
+
+;;; hippie-namespace-reload (arg)
+
+;;; hippie-namespace-add (namespace)
+
+;;; todo test the advised hippie-expand command
 
 ;;
 ;; Emacs
