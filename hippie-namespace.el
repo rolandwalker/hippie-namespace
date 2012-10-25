340e82411c8f0317a75ccd6e2073ce89666c0f14
Roland Walker <walker@pobox.com>
2012-10-25 11:16:27 -0400

    bump version

--- hippie-namespace.el
+++ hippie-namespace.el
@@ -5,8 +5,8 @@
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/hippie-namespace
 ;; URL: http://raw.github.com/rolandwalker/hippie-namespace/master/hippie-namespace.el
-;; Version: 0.5.4
-;; Last-Updated: 14 Sep 2012
+;; Version: 0.5.6
+;; Last-Updated: 25 Oct 2012
 ;; EmacsWiki: HippieNamespace
 ;; Keywords: convenience, lisp, tools, completion
 ;;
@@ -177,7 +177,7 @@
 ;;;###autoload
 (defgroup hippie-namespace nil
   "Special treatment for namespace prefixes in `hippie-expand'."
-  :version "0.5.4"
+  :version "0.5.6"
   :link '(emacs-commentary-link "hippie-namespace")
   :prefix "hippie-namespace-"
   :group 'hippie-expand
